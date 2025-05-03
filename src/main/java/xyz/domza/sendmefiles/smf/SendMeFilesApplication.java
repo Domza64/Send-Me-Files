5084bf2761ca2b1b2d128e04818c8d586aadd556
Domza64 <juricicdominik@protonmail.com>
2025-05-03 19:09:58 +0200

    Initialize project

---
+++ src/main/java/xyz/domza/sendmefiles/smf/SendMeFilesApplication.java
@@ -0,0 +1,13 @@
+package xyz.domza.sendmefiles.smf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMeFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendMeFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/domza/sendmefiles/smf/SendMeFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.domza.sendmefiles.smf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMeFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
